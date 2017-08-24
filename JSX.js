2544c3b3f5ddec018a6145046bd481f40c9d7280
Muhammet <muhammeta7@gmail.com>
2017-08-24 15:36:57 -0400

    nesting elements and JSX outer elements

--- JSX.js
+++ JSX.js
@@ -15,3 +15,40 @@ const myArticle = <article>Something</article>
 // elements can. my-attribute-name="my-attribute-value"
 const p1 = <p id="large">foo</p>;
 const p2 = <p id="small">bar</p>;
+
+// Nested JSX 
+ const theExample = (
+   <a href="https://www.example.com">
+     <h1>
+       Click me!
+     </h1>
+   </a>
+);
+
+const myDiv = (
+  <div>
+    <h1>Hello world</h1>
+  </div>
+);
+
+// JSX Outer Elements
+// There's a rule that we haven't mentioned: a JSX expression 
+// must have exactly one outermost element.
+const paragraphs = (
+  <div id="i-am-the-outermost-element">
+    <p>I am a paragraph.</p>
+    <p>I, too, am a paragraph.</p>
+  </div>
+);
+
+const blog = (
+  <div>
+    <img src="pics/192940u73.jpg" />
+    <h1>
+      Welcome to Dan's Blog!
+    </h1>
+    <article>
+      Wow I had the tastiest sandwich today.  I <strong>literally</strong> almost freaked out.
+    </article>
+  </div>
+);
