ce66f88ad823a11a53fd4783f747f56b74977378
Muhammet <muhammeta7@gmail.com>
2017-08-24 15:30:11 -0400

    intro to jsx elements and attributes

---
+++ JSX.js
@@ -0,0 +1,17 @@
+// If a JavaScript file contains JSX code, 
+// then that file will have to be compiled. 
+// That means that before the file reaches a web browser,
+// a JSX compiler will translate any JSX into regular JavaScript.
+
+// JSX Elements: This JSX element looks exactly like HTML!
+const p = <p>Hello world</p>;
+
+// JSX Elements and Their Surroundings
+// const is a variable and can be used anywhere
+const myArticle = <article>Something</article>
+
+// Attributes in JSX
+// JSX elements can have attributes,// just like HTML 
+// elements can. my-attribute-name="my-attribute-value"
+const p1 = <p id="large">foo</p>;
+const p2 = <p id="small">bar</p>;
